f34b46df12ed93cdb798e8d20908f76cee205703
Animesh <103233977+Animesh-2211@users.noreply.github.com>
2024-08-05 17:05:42 +0530

    Time: 1 ms (99.79%), Space: 53.7 MB (7.52%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,26 +1,39 @@
 class Solution {
     public int majorityElement(int[] nums) {
 
-        //MOORE VOTING ALGORITHM
+        // MOORE VOTING ALGORITHM
 
-        int candidate=0;
-        int count=0;
-        int n=nums.length;
-        for(int i=0;i<n;i++){
-            if(count==0){
-                candidate=nums[i];
-                count=1;
-            }
-            else if(nums[i]==candidate){
+        int candidate = 0;
+        int count = 0;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (count == 0) {
+                candidate = nums[i];
+                count = 1;
+            } else if (nums[i] == candidate) {
                 count++;
             }
 
-            else{
+            else {
                 count--;
             }
         }
 
-        return candidate;
+        // return candidate; we can stop here only the code will work but we should also
+        // verify according to this algo
+
+        count = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == candidate) {
+                count++;
+            }
+        }
+
+        if (count > (n / 2)) {
+            return candidate;
+        }
+
+        return -1;
 
     }
 }
