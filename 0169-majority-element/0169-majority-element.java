838c1b967f5e935225e0fea1878aa820b59d9e4e
Animesh <103233977+Animesh-2211@users.noreply.github.com>
2024-08-05 17:02:40 +0530

    Time: 1 ms (99.79%), Space: 53 MB (35.36%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,24 +1,26 @@
-import java.util.HashMap;
-
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> mp=new HashMap<>();
-            int count=0;
-            for(int i=0;i<nums.length;i++){
-                if(mp.containsKey(nums[i])){
-                    mp.put (nums[i],mp.get(nums[i])+1);
-                }
-                else{
-                    mp.put(nums[i],1);
-                }
+
+        //MOORE VOTING ALGORITHM
+
+        int candidate=0;
+        int count=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(count==0){
+                candidate=nums[i];
+                count=1;
             }
-            int majority=nums.length/2;
-            for( int num:nums){
-                if(mp.get(num)>majority){
-                    count=num;
-                    break;
-                }
+            else if(nums[i]==candidate){
+                count++;
             }
-            return count;
+
+            else{
+                count--;
+            }
         }
+
+        return candidate;
+
     }
+}
