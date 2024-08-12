15c94596b421a12e2cd7c8955e84ec6827f2956b
Animesh <103233977+Animesh-2211@users.noreply.github.com>
2024-08-13 01:34:26 +0530

    Time: 4 ms (6.46%), Space: 59.2 MB (47.68%) - LeetHub

---
+++ 0932-monotonic-array/0932-monotonic-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        boolean asc=true;
+        boolean desc=true;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]<nums[i+1]){
+                asc=false;
+            }
+        }
+             for(int i=0;i<nums.length-1;i++){
+               if(nums[i]>nums[i+1]){
+                desc=false;
+
+
+    }
+}
+    return asc || desc;
+
+
+    }}
