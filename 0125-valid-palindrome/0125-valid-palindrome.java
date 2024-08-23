49588be00a6f2267d9f7bbdb0e8873a4cd3eae8e
Animesh <103233977+Animesh-2211@users.noreply.github.com>
2024-08-23 19:20:28 +0530

    Time: 2 ms (99.13%), Space: 42.8 MB (83.66%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -3,11 +3,34 @@ class Solution {
         // s=s.replaceAll("\\s","");
         // s=s.replaceAll(",","");
         // s=s.replaceAll(":","");
-        s=s.replaceAll("[^A-Za-z0-9]","");
-        s=s.toLowerCase();
-        StringBuilder str=new StringBuilder(s);
-        str=str.reverse();
-        // str.toString();
-        return s.equals(str.toString()); 
+    //     s=s.replaceAll("[^A-Za-z0-9]","");
+    //     s=s.toLowerCase();
+    //     StringBuilder str=new StringBuilder(s);
+    //     str=str.reverse();
+    //     // str.toString();
+    //     return s.equals(str.toString()); 
+ 
+    int n=s.length();
+    int left=0;
+    int right=s.length()-1;
+
+    while(left<right){
+
+        while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
+            left++;
+        }
+
+         while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
+            right--;
+        }
+
+        if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+            return false;
+        }
+        left++;
+        right--;
     }
+        return true;
+
+}
 }
