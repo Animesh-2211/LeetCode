85cf637cf5e2ace0e9a9d589781ef897b71a9fee
Animesh <103233977+Animesh-2211@users.noreply.github.com>
2025-01-17 11:50:24 +0530

    Time: 2 ms (100%), Space: 62.4 MB (40.59%) - LeetHub

---
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean doesValidArrayExist(int[] derived) {
+        int total=0;
+        for(int i=0;i<derived.length;i++){
+            total^=derived[i];
+
+        }
+        return(total==0);
+    }
+}
