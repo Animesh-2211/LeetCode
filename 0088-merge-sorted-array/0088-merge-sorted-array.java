d8f042b4a3c6d9c46d3a4e64482389a161cdd165
Animesh <103233977+Animesh-2211@users.noreply.github.com>
2025-09-11 00:39:15 +0530

    Time: 0 ms (100%), Space: 42 MB (90.63%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,18 +1,19 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int index=m-1;
-        int point=n-1;
-        int merge=m+n-1;
-        while(index>=0 && point>=0){
-        if(nums1[index] > nums2[point]){
-            nums1[merge--]= nums1[index--];
-        }else{
-            nums1[merge--]= nums2[point--];
-        }
-    }
-    while(point>=0){
-        nums1[merge--]=nums2[point--];
-    }
-  
+         int index = m-1;
+         int point = n-1;
+         int co = m+n-1;
+
+         while(index>=0 && point>=0){
+            if(nums1[index] > nums2[point]){
+                nums1[co--] = nums1[index--];
+            }
+            else{
+                nums1[co--] = nums2[point--];
+            }
+         }
+         while(point>=0){
+            nums1[co--] = nums2[point--];
+         }
     }
 }
