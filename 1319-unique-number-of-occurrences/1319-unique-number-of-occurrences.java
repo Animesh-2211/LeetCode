a4ffbbb7a0d3076b5ff46405e8a18c6b006a9aec
Animesh <103233977+Animesh-2211@users.noreply.github.com>
2025-09-11 00:49:22 +0530

    Time: 2 ms (98.46%), Space: 41.8 MB (68.94%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -1,23 +1,16 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
+         HashMap<Integer,Integer> mp = new HashMap<>();
+         for(int i=0;i< arr.length;i++){
+            mp.put(arr[i],mp.getOrDefault(arr[i],0)+1);
 
-      HashMap<Integer,Integer> mp=new HashMap<>();
-
-      for(int i=0;i<arr.length;i++){
-        mp.put(arr[i],mp.getOrDefault(arr[i],0)+1);    
-    }
-
-      HashSet<Integer> set=new HashSet<>();
-
-
-    for(int freq:mp.values()){
-     
-      if(!set.add(freq)){
-        return false;
-      }
-
-    }
-    return true;
-        
+         }
+         HashSet <Integer> set= new HashSet<>();
+         for(int i : mp.values()){
+            if(!set.add(i)){
+                return false;
+            }
+         }
+         return true;
     }
 }
