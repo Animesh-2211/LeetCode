454283b0e7bb088a454a28e85c61b9ea4b3c8fab
Animesh <103233977+Animesh-2211@users.noreply.github.com>
2025-09-11 00:42:34 +0530

    Time: 0 ms (100%), Space: 48.7 MB (73.52%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,29 +1,19 @@
 class Solution {
     public void reverseString(char[] s) {
-        // Stack<Character> st=new Stack<>();
-        // for(int i=0;i<s.length;i++){
-        //     st.add(s[i]);
-        // }
-        // for(int j=0;j<s.length;j++){
-        //     s[j]=st.pop();
-        // }
+        int left =0;
+        int right= s.length-1;
+
+        while(left<right){
+            swap(left,right,s);
+            right--;
+            left++;
+        }
+    }
+        public void swap(int left,int right,char[] s){
+            char ch = s[left];
+            s[left] = s[right];
+            s[right] = ch;
+        }
         
     
-     int n=s.length;
-    int left=0;
-    int right=n-1;
-    
-    while(left <right){
-        swap(s,left,right);
-        left++;
-        right--;
-    }
-    
-    }
-
-    public void swap( char[] s,int a,int b){
-        char ch=s[a];
-        s[a]=s[b];
-        s[b]=ch;
-    }
 }
